7857678214fe25d81613d147c5e307a14694312b
tunerok <tunerok93@gmail.com>
2026-06-19 01:00:44 +0300

    Initial Commit

---
+++ HexMac/HexMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexMacApp.swift
+//  HexMac
+//
+//  Created by Артем on 19.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct HexMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
